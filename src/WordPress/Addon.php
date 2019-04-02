ffd0000a5c52de718399353cd80177384f12e5b2
Pereira Ricardo <r.pereira@madeinitalyslc.it>
2019-04-02 23:22:00 +0200

    Remove useless code

--- src/WordPress/Addon.php
+++ src/WordPress/Addon.php
@@ -9,15 +9,4 @@ use NunoCodex\Slumex\Container\Container;
  * @package NunoCodex\Slumex\WordPress
  */
 class Addon extends Container implements AddonInterface
-{
-    /**
-     * Addon constructor.
-     * @param array $values
-     */
-    public function __construct(array $values = [])
-    {
-        foreach ($values as $k => $v) {
-            $this->offsetSet($k, $v);
-        }
-    }
-}
+{ }
